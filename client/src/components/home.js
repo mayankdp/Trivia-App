0e272d74d5b660469999e46e21057a0561038481
Henry Moore <henrypmoore123@gmail.com>
2022-04-12 13:41:13 -0400

    Small formatting update

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,6 +1,6 @@
 export default function Home() {
     return (
-        <div id="page_placeholder">
+        <div id="home_page" className="page_placeholder">
             <h2>Home Placeholder</h2>
         </div>
     );
